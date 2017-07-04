97088ef01f168dce5a73049d01f1a5a569c7b4e6
Brendan Molloy <brendan@bbqsrc.net>
2017-07-04 19:02:20 +1000

    Use PostgreSQL

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -3,7 +3,6 @@ import com.natpryce.konfig.getValue
 import com.natpryce.konfig.stringType
 
 object database : PropertyGroup() {
-    val uri by stringType
     val username by stringType
     val password by stringType
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,21 @@
 import com.natpryce.konfig.ConfigurationProperties
-import org.h2.jdbcx.JdbcConnectionPool
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import javax.sql.DataSource
 
+fun newDataSource(config: ConfigurationProperties): DataSource {
+    var cfg = HikariConfig()
+
+    cfg.dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
+    cfg.username = config[database.username]
+    cfg.password = config[database.password]
+
+    return HikariDataSource(cfg)
+}
 
 fun main(args: Array<String>) {
     val config = ConfigurationProperties.fromResource("config.properties")
-
-    val pool = JdbcConnectionPool.create(
-            config[database.uri],
-            config[database.username],
-            config[database.password]
-    )
+    val pool = newDataSource(config)
 
     // initDb(pool)
     initRoutes(pool)
